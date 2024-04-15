7b719ac46589a6950e599035d3ba03943e04708c
leonMacMini <leonhan1992@163.com>
2024-04-15 08:42:16 +0800

    bug fix

---
+++ src/main/java/com/hanqi/leetcode/FirstMissingPositive.java
@@ -0,0 +1,29 @@
+package com.hanqi.leetcode;
+
+public class FirstMissingPositive {
+    public static int firstMissingPositive(int[] nums) {
+        int n = nums.length;
+        for (int i = 0; i < n; ++i) {
+            if (nums[i] <= 0) {
+                nums[i] = n + 1;
+            }
+        }
+        for (int i = 0; i < n; i++) {
+            int num = Math.abs(nums[i]);
+            if (num <= n) {
+                nums[num - 1] = -Math.abs(nums[num - 1]);
+            }
+        }
+        for (int i = 0; i < n; ++i) {
+            if (nums[i] > 0) {
+                return i + 1;
+            }
+        }
+        return n + 1;
+    }
+
+    public static void main(String[] args) {
+        int[] array = new int[]{3,4,-1,1};
+        System.out.println(firstMissingPositive(array));
+    }
+}
